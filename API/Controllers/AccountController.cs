7ba447a939b56ea7536fe8a5a0e983d128b5bca6
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2024-06-26 00:37:37 +0300

    extending the user entity, generating seed data, repository pattern and automapper

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -14,8 +14,8 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     [HttpPost("register")] // account/register
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
         if(await UserExists(registerDto.Username)) return BadRequest("Username is taken!");
-
-        using var hmac = new HMACSHA512();
+        return Ok();
+        /* using var hmac = new HMACSHA512();
 
         var user = new AppUser{
             UserName = registerDto.Username.ToLower(),
@@ -29,7 +29,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         return new UserDto{
             Username = user.UserName,
             Token = tokenService.CreateToken(user)
-        };
+        }; */
     }
 
     [HttpPost("login")]
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,23 +1,27 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-        var users = await context.Users.ToListAsync();
-
-        return users;
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+        var users = await userRepository.GetMembersAsync();
+        return Ok(users);
     }
 
-    [HttpGet("{id:int}")] // /api/users/id
-    public async Task<ActionResult<AppUser>> GetUser(int id){
-        var user = await context.Users.FindAsync(id);
+    [HttpGet("{username}")] // /api/users/id
+    public async Task<ActionResult<MemberDto>> GetUser(string username){
+        var user = await userRepository.GetMemberAsync(username);
 
         if(user == null) return NotFound();
         
