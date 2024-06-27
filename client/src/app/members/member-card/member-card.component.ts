c405baa63e1577b867e6c33474faf52153ed1efd
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2024-06-27 16:33:29 +0300

    edit profile, loading indicators, data cache

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, input } from '@angular/core';
 import { Member } from '../../_models/member';
-import {RouterLink } from '@angular/router';
+import {RouterLink} from '@angular/router';
 
 
 @Component({
