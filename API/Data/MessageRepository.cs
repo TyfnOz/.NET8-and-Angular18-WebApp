92f7c2eadddf44d3aff5d2f823a5cb4d097d3adc
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2024-07-08 21:55:04 +0300

    polishing realtime messaging

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -69,22 +69,20 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
 
     public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
     {
-        var messages = await context.Messages
+        var query = context.Messages
             .Where(
                 x => x.RecipientUsername == currentUsername && x.RecipientDeleted == false && x.SenderUsername == recipientUsername ||
                 x.SenderUsername == currentUsername && x.SenderDeleted == false && x.RecipientUsername == recipientUsername)
-            .OrderBy(x => x.MessageSent)
-            .ProjectTo<MessageDto>(mapper.ConfigurationProvider)
-            .ToListAsync();
+            .OrderBy(x => x.MessageSent).AsQueryable();
 
-        var unreadMessages = messages.Where(x => x.DateRead == null && 
+        var unreadMessages = query.Where(x => x.DateRead == null && 
             x.RecipientUsername == currentUsername).ToList();
         
         if(unreadMessages.Count != 0){
             unreadMessages.ForEach(x => x.DateRead = DateTime.UtcNow);
         }
 
-        return messages;
+        return await query.ProjectTo<MessageDto>(mapper.ConfigurationProvider).ToListAsync();
     }
 
     public void RemoveConnection(Connection connection)
